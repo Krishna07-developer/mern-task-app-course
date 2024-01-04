d69a17346730337980853033375cbb17e201f900
unpairedkrish-7 <krishnabhukya893@gmail.com>
2024-01-04 11:54:58 +0530

    cors changed

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ const app = express();
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
-app.use(cors())
+app.use(cors({
+    origin : ["http://localhost:3000"]
+}))
 
 
 app.get("/" , async (req,res)=>{
