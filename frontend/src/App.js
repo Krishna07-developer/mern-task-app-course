21ad154eaae90da1d1053cd9b40fb90c681b5f4a
unpairedkrish-7 <krishnabhukya893@gmail.com>
2024-01-04 11:53:56 +0530

    Final changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import TaskList from "./components/TaskList";
-import { ToastContainer , toast} from 'react-toastify';
+import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
--- frontend/src/components/TaskForm.js
+++ frontend/src/components/TaskForm.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const TaskForm = ({createTask , name , handleInputChange}) => {
-  return <form className='task-form' onSubmit={createTask}>
+const TaskForm = ({createTask , name , handleInputChange , isEditing , updateTask}) => {
+  return <form className='task-form' onSubmit={isEditing ? updateTask : createTask}>
     <input type="text" placeholder='Add a Task' name='name' value={name} onChange={handleInputChange}/>
-    <button type='submit'>Add</button>
+    <button type='submit'>{isEditing ? "Edit" : "Add"}</button>
   </form>
 }
 
